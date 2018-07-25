e16cfc49a82677d0624de1367b3bd770cf06fd6d
Hoyoung Jung <honux77@gmail.lcom>
2018-07-25 15:10:54 +0900

    프로그래머스 레벨 1 문제 풀이
    가운데 문자열 구하기

---
+++ programmers/100-1.js
@@ -0,0 +1,14 @@
+//프로그래머스 레벨1 알고리즘
+//문자열의 가운데 문자 리턴하기
+
+function solution(s) {
+    var answer = '';
+    var middle = Math.floor(s.length / 2);
+    
+    if (s.length % 2 == 0) {
+        answer = s[middle - 1] + s[middle]
+    } else {
+        answer = s[middle];
+    }
+    return answer;
+}
